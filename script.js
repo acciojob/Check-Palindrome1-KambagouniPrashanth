fb78d0b8a7d6102ec532de540a0cfdd834f43174
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-29 07:11:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-	return str == str.split("").reverse().join("");
+	return str == str.split("").reverse().join(""); let new_arr=str.split("")
+    // console.log(new_arr)
+    new_arr.reverse();
+    // console.log(new_arr)
+    let s=new_arr.join("")
+
+    // console.log(s)
+    // console.log(str==s)
+
+	return str==s;
 
 }
 module.exports = palindrome
